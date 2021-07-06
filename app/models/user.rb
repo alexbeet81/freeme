01f4e81cc0c3af3d787f8c889866a0e1737f6c8f
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-06 21:27:53 +0800

    changed locaiton to location in experience talbe - added basic frontend to test controllers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :experience, dependent: :destroy
 
-  validates :email, uniquness: true
+  # validates :email, uniquness: true
 end
