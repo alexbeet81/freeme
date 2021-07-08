fb1341b6a5f9f88fc9b9eed1527ac3dddc53b5db
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-08 20:39:33 +0800

    updated skills controller and users controller - added create funciton to skills controller.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,19 @@
 class SkillsController < ApplicationController
+
+  def create
+    @skill = Skill.new(skill_params)
+
+    if @skill.save
+      redirect_to user_path(@user)
+    else
+      # Can't render show - where should this lead to?
+      render :show
+    end
+  end
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:skill_name, :level, :years_of_experience)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,36 +2,22 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    @skill = Skill.new(skill_params)
+    @skill = Skill.new
     # show only the current users skills
     @skills = Skill.where(user_id: @user.id)
     @edit_skill = Skill.(params[:id])
 
-    @project = Project.new(project_params)
+    @project = Project.new
     @projects = Project.where(user_id: @user.id)
 
-    @experience = Experience.new(experience_params)
+    @experience = Experience.new
     @experiences = Experience.where(user_id: @user.id)
-  end
-
-  def update
-    @edit_skill.update(skill_params)
-
-    if @edit_skill.save
-      redirect_to user_path(@user)
-    else
-      render :show
-    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def skill_params
-    params.require(:skill).permit(:skill_name, :level, :years_of_experience)
   end
 
   def project_params
