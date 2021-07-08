b91f1c98fcb8d2ff2f663784a3bf60666ec48ae3
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-08 21:01:04 +0800

    set user and expereince in expereince controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,4 +1,6 @@
 class ExperiencesController < ApplicationController
+  before_action :set_user
+  before_action :set_experience, only: [:destroy, :update]
   def create
     @experience = Experience.new(experience_params)
 
@@ -25,7 +27,6 @@ class ExperiencesController < ApplicationController
       render 'users/show'
     end
   end
-
 
   private
 
