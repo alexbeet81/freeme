68f12bffdd0148ee2e7f19db08324f9f453c2410
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-08 20:41:41 +0800

    added create to project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,18 @@
 class ProjectsController < ApplicationController
+  def create
+    @project = Skill.new(project_params)
+
+    if @project.save
+      redirect_to user_path(@user)
+    else
+      # Can't render show - where should this lead to?
+      render 'users/show'
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :decription)
+  end
 end
