911e1b67647250668316afd39e308a0062600a72
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-15 20:41:21 +0800

    added profile to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  skip_before_action :authenticate_user!, only: :show
+  skip_before_action :authenticate_user!, only: [:show, :profile]
 # Paul added to get to user profile page to edit
 
   def show
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
 
     @new_experience = Experience.new
     @experiences = Experience.where(user_id: @user.id)
+  end
+
+  def profile
+    @user = User.find(params[:user_id])
+    authorize @user
   end
 
   private
