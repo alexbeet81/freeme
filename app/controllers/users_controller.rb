de8509c82895ad2c9159c9cd09e9f7fe395d618a
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-17 12:58:53 +0800

    updated users_controller showcase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :set_visitor, only: [:showcase]
+  before_action :set_projects
+  before_action :set_skills
+  before_action :set_experiences
+
   skip_before_action :authenticate_user!, only: [:show, :showcase]
 # Paul added to get to user profile page to edit
 
@@ -7,27 +12,34 @@ class UsersController < ApplicationController
     authorize @user
 
     @new_skill = Skill.new
-    # show only the current users skills
-    @skills = Skill.where(user_id: @user.id)
-    # @edit_skill = Skill.find(params[:id])
 
     @new_project = Project.new
-    @projects = Project.where(user_id: @user.id)
 
     @new_experience = Experience.new
-    @experiences = Experience.where(user_id: @user.id)
   end
 
-  def showcase
-    @user = User.find(params[:user_id])
-    authorize @user
-
-    @skills = Skill.where(user_id: @user.id)
-  end
+  def showcase; end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def set_visitor
+    @user = User.find(params[:user_id])
+    authorize @user
+  end
+
+  def set_skills
+    @skills = Skill.where(user_id: @user.id)
+  end
+
+  def set_projects
+    @projects = Project.where(user_id: @user.id)
+  end
+
+  def set_experiences
+    @experiences = Experience.where(user_id: @user.id)
   end
 end
