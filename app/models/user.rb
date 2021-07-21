75674b0e83336bdc9618ed855aa70fafe990ee42
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-21 12:27:57 +0800

    set_slug added to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_validation :set_slug, only: [:create, :update]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -10,4 +12,10 @@ class User < ApplicationRecord
 
   # validates :email, uniquness: true
   has_one_attached :photo
+
+  private
+
+  def set_slug
+    self.slug = first_name.to_s.parameterize
+  end
 end
