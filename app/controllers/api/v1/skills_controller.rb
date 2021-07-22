0fbd4d1a0448f529ace9928c8deeeaf95f12bbb0
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-22 21:19:02 +0800

    working for now - needs more updates

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::SkillsController < Api::V1::BaseController
   # acts_as_token_authentication_handler_for User, only: [:update]
   before_action :set_skill, only: [:update]
+  before_action :set_user, only: [:update]
 
   def update
     if @skill.update(skill_params)
-      render 'users/show'
+      redirect_to api_v1_user_path(@user)
+      # render 'users/show'
     else
       render_error
     end
@@ -23,5 +25,10 @@ class Api::V1::SkillsController < Api::V1::BaseController
 
   def render_error
     render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def set_user
+    @user = User.where(id: @skill.user_id)
+    binding.pry
   end
 end
