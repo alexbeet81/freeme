ea4f6a1c9a13d0b27049951217619aca7c098207
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-22 15:08:44 +0800

    created show view - user displays skills, projects and expereinces:

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
-    @users = policy_scope(users)
+    @users = policy_scope(User)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
