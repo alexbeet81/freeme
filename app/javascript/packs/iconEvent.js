2910528dec5ba840c05bfcdd2b9bc19ff5e652f1
Paul Desmond <pdesmond27jr@gmail.com>
2021-07-24 00:19:31 -0400

    401 error iconfinder

--- app/javascript/packs/iconEvent.js
+++ app/javascript/packs/iconEvent.js
@@ -5,10 +5,14 @@ const fetch = require('node-fetch')
 // input.addEventListener('something', () => { } )
 function getIcon(keyWord){
   fetch(`https://api.iconfinder.com/v4/icons/search?query=${keyWord}&count=10&premium=false&vector=false`, {
+    mode: 'no-cors',
   headers: {
     'Content-Type': 'application/json',
-     'Authorization': 'Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1',
-     'origin': "https://developer.iconfinder.com"
+     'Authorization': 'Bearer 4OgPhxvQmirUXTmHXvsh21b5Vfs9McVukloqQVrVl2v71ith1cIdw4zH23sfi1UC'
+  },
+  data: {
+    grant_type: 'client_credentials',
+    client_id: 'T6vkCg4hfpOABsf2oWMlIgwdUX8vdFrXkneHXGDjLUVj5eGK3rqdAUE0ojGSyA2f'
   }
 })
 .then(response => response.json())
