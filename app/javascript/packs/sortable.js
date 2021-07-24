6114b7a1b5d27738d615ac8e1fa7299175c77cb5
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-24 10:28:42 +0800

    testing api PATCH request in sortable.js

--- app/javascript/packs/sortable.js
+++ app/javascript/packs/sortable.js
@@ -9,16 +9,40 @@ const initSortable = () => {
     onEnd: (event) => {
       // fetch POST
       // alert(`${event.oldIndex} moved to ${event.newIndex}`);
-      fetch(`http://localhost:3000/api/v1/users/5-dennis_bergkamp`)
-      .then(res => res.json())
-      .then(res => console.log(res))
-
+      // fetch(`http://localhost:3000/api/v1/users/5-dennis_bergkamp`)
+      // .then(res => res.json())
+      // .then(res => console.log(res))
+      //
     }
   });
 
   document.querySelector('#save-skill').addEventListener('click', (e) => {
     let order = sortable.toArray();
-    console.log(order)
+    // console.log(order)
+    order.forEach( e => console.log(e))
+    // fetch(`http://localhost:3000/api/v1/users/5-dennis_bergkamp`)
+    //   .then(res => res.json())
+    //   .then(res => res.skills.forEach( skill => {
+    //     skill.level = "test";
+    //   }))
+    fetch(`http://localhost:3000/api/v1/users/5-dennis_bergkamp`, {
+      method: 'PATCH',
+      headers: {
+        "Content-type": "application/json",
+        "X-User-Email": "dennis@berkamp.com",
+        "X-User-Token": "SszxUGurVaYpqh4_wUW6"
+      },
+      body: JSON.stringify({
+        headline: "I HATE football!"
+      })
+    })
+    .then( response => response.json())
+    .then( response => console.log(response.headline))
+    // do a patch request
+    // itterate through the skills.
+    // itterate through the order array.
+    // set position to equal the position in the array
+
   })
 };
 
