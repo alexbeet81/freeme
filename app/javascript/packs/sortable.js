486f2ad00692ea971d049b9c9794419e322c5f8d
Alex Pritchard <alexbeet1234@gmail.com>
2021-07-24 19:02:58 +0800

    created experiences controller and updated policy

--- app/javascript/packs/sortable.js
+++ app/javascript/packs/sortable.js
@@ -15,6 +15,12 @@ const initSortable = () => {
     animation: 150,
   });
 
+  const sortExperience = document.querySelector('#sort-experience');
+
+  let sortableExperience = Sortable.create(sortExperience, {
+    ghostClass: "ghost",
+    animation: 150,
+  })
 
   document.querySelector('#save-skill').addEventListener('click', (e) => {
     let order = sortableSkill.toArray();
@@ -56,6 +62,26 @@ const initSortable = () => {
       body: JSON.stringify({
         position: newPositions
       })
+    })
+  })
+
+  document.querySelector('#save-experience').addEventListener('click', (e) => {
+    let order = sortExperience.toArray();
+
+    const newPositions = []
+
+    document.querySelectorAll('#sort-experience li').forEach((e, index) => {
+      newPositions.push({project: e.dataset.project, position: index})
+    })
+  })
+
+  fetch(`http://localhost:3000/api/v1/experience`, {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      position: newPositions
     })
   })
 };
