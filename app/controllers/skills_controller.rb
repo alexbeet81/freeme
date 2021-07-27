6bca8581c83fb2ecc1aa0f6a1df693dd8e5f532d
Paul Desmond <pdesmond27jr@gmail.com>
2021-07-27 11:47:56 -0400

    ls not working

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -10,11 +10,13 @@ class SkillsController < ApplicationController
     @icon = Icon.find_by_name(@skill[:skill_name].downcase)
 
     if @icon.nil?
-      @skill.icon = @icon.where(id: 1)
+      @skill.icon = Icon.find_by(id: 1)
     else
       @skill.icon = @icon
     end
-
+    # unless @icon.nil?
+    #   @skill.icon = @icon
+    # end
 
     if @skill.save
       redirect_to (user_path(@skill.user) + "#skill-#{@skill.id}")
