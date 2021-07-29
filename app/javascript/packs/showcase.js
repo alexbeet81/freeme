dbab89d11f4029037ac99973b566feadea3ac62a
Paul Desmond <pdesmond27jr@gmail.com>
2021-07-29 10:29:14 -0400

    on master changing branches

---
+++ app/javascript/packs/showcase.js
@@ -0,0 +1,15 @@
+
+function showcaseAni(){
+
+    let skillsGrid = document.querySelector('.sc-skills-grid');
+    let introPos = skillsGrid.getBoundingClientRect().top;
+    console.log(introPos);
+    // let screenPos = window.innerHeight;
+
+    // if(introPos < screenPos){
+    //   skillsGrid.classlist.add()
+    // }
+
+};
+showcaseAni();
+export { showcaseAni };
