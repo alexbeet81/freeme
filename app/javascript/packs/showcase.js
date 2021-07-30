0aaef6eaff58947f947a55559d249304b199fe73
Paul Desmond <pdesmond27jr@gmail.com>
2021-07-30 08:32:57 -0400

    showcase fade in js added on skill, project, exp cards

--- app/javascript/packs/showcase.js
+++ app/javascript/packs/showcase.js
@@ -2,14 +2,27 @@
 function showcaseAni(){
 
     let skillsGrid = document.querySelector('.sc-skills-grid');
-    let introPos = skillsGrid.getBoundingClientRect().top;
-    console.log(introPos);
-    // let screenPos = window.innerHeight;
+    let projGrid = document.querySelector('.sc-project-card');
+    let expGrid = document.querySelector('.sc-exp-grid');
+    let skillPos = skillsGrid.getBoundingClientRect().top;
+    let projPos = projGrid.getBoundingClientRect().top;
+    let expPos = expGrid.getBoundingClientRect().top;
 
-    // if(introPos < screenPos){
-    //   skillsGrid.classlist.add()
-    // }
+    let screenPos = window.innerHeight;
+
+    if(skillPos < screenPos){
+      skillsGrid.classList.add('skill-appear')
+    }
+
+    if(projPos < screenPos){
+      projGrid.classList.add('pro-appear')
+    }
+
+    if(expPos < screenPos){
+      expGrid.classList.add('exp-appear')
+    }
 
 };
-showcaseAni();
+
+window.addEventListener('scroll', showcaseAni);
 export { showcaseAni };
