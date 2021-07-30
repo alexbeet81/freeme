e30f34d7c63a160996c58c00d570674c2b0f9c75
Paul Desmond <pdesmond27jr@gmail.com>
2021-07-30 09:20:21 -0400

    showcase fade edited

--- app/javascript/packs/showcase.js
+++ app/javascript/packs/showcase.js
@@ -2,24 +2,15 @@
 function showcaseAni(){
 
     let skillsGrid = document.querySelector('.sc-skills-grid');
-
-    let introPos = skillsGrid.getBoundingClientRect().top;
-    console.log(introPos);
-    // let screenPos = window.innerHeight;
-
-    // if(introPos < screenPos){
-    //   skillsGrid.classlist.add()
-    // }
-
-
-
     let projGrid = document.querySelector('.sc-project-card');
     let expGrid = document.querySelector('.sc-exp-grid');
+
     let skillPos = skillsGrid.getBoundingClientRect().top;
     let projPos = projGrid.getBoundingClientRect().top;
     let expPos = expGrid.getBoundingClientRect().top;
 
     let screenPos = window.innerHeight;
+
 
     if(skillPos < screenPos){
       skillsGrid.classList.add('skill-appear')
