951622b6cbcf7152622476ac3065152f3262455d
Alex Pritchard <alexbeet1234@gmail.com>
2023-08-04 12:24:04 +0100

    ran updates

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.0'
+ruby '3.0.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
@@ -24,6 +24,7 @@ gem 'redis', '~> 4.0'
 gem 'open-uri'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'stringio', '3.0.1'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_044559) do
+ActiveRecord::Schema.define(version: 2021_07_27_124633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +43,6 @@ ActiveRecord::Schema.define(version: 2021_07_30_044559) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "chats", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "experiences", force: :cascade do |t|
     t.string "experience_name"
     t.string "company"
@@ -69,16 +63,6 @@ ActiveRecord::Schema.define(version: 2021_07_30_044559) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.bigint "chat_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["chat_id"], name: "index_messages_on_chat_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -122,7 +106,6 @@ ActiveRecord::Schema.define(version: 2021_07_30_044559) do
     t.string "slug"
     t.string "authentication_token", limit: 30
     t.boolean "available", default: true
-    t.boolean "guest", default: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -131,8 +114,6 @@ ActiveRecord::Schema.define(version: 2021_07_30_044559) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "experiences", "users"
-  add_foreign_key "messages", "chats"
-  add_foreign_key "messages", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "skills", "icons"
   add_foreign_key "skills", "users"
